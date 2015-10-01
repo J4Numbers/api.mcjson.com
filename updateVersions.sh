994681cb14cae08fff0e839f27a3d4b5979a0085
root <root@tehbeard.com>
2015-10-01 10:27:37 -0400

    Add update script to repo

---
+++ updateVersions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd data
+git pull
+cd ../
+node scripts/importVersionsFromMojang.js
+cd data
+git add .
+echo "auto updating versions" > ../ver-commit.txt
+git status --porcelain >> ../ver-commit.txt
+git commit -F ../ver-commit.txt
+rm ../ver-commit.txt
+git push
