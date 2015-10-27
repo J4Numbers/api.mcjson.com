4be017faa5df76f5aceee38db6eba3c065978941
James <beardyjames@live.co.uk>
2015-10-27 22:54:44 +0000

    Dump out metadata, will make into editable in future.

--- editor/js/app/page/editors/ItemEditor.js
+++ editor/js/app/page/editors/ItemEditor.js
@@ -2,11 +2,19 @@ import * as View from 'mvc/View';
 export default class ItemEditor extends View {
 	constructor(model){
 		super();
-		this.model = 'model';
-		this.el.innerHTML = 'Item editor goes here.';
+		this.model = model;
+		this.el.innerHTML = `
+
+		<div class="metadata">
+		</div>`;
 	}
 
 	render(){
-
+		this.qs(".metadata").innerHTML = this.model.get('meta').map((meta)=>{
+			return `<div class="meta-category" id="meta-category-${meta.label}">
+			<h3>${meta.label}</h3>` +
+			meta.entries.map((entry)=>`<div><strong>${entry.label}</strong> : ${entry.value}</div>`).join("\n") +
+			`</div>`;
+		}).join("\n");
 	}
 }
