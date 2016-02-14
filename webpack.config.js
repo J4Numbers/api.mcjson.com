988259bc4c9fb04c77bfa38f5c45777af7f47127
James <beardyjames@live.co.uk>
2016-02-14 16:33:03 +0000

    tidy up config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 var path = require('path');
 module.exports = {
-	resolve: {
-		root: ['./editor/js/app'].map((p)=>path.resolve(p)),
-		alias: {
-			'mvc':'es6mvc'
-		}
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js?$/,
-				loader: 'babel-loader',
-				query:{
-					presets: ['es2015']
-				}
-			},
-			{
-	        test: /.jsx?$/,
-	        loader: 'babel-loader',
-	        exclude: /node_modules/,
-	        query: {
-	          presets: ['es2015', 'react']
-	        }
-      }
-		]
-	},
-	entry: "app.js",
+    resolve: {
+        root: ['./editor/js/app'].map((p) => path.resolve(p)),
+        alias: {
+            'mvc': 'es6mvc'
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    entry: "app.js",
     output: {
         path: __dirname + "/editor/assets",
         filename: "bundle.js"
