2c9f0b46aa88123cd4b4963681ba42ee72c96f70
James <beardyjames@live.co.uk>
2016-02-16 23:52:27 +0000

    Fix version fetching script.

--- scripts/importVersionsFromMojang.js
+++ scripts/importVersionsFromMojang.js
@@ -3,7 +3,6 @@ var path = require('path');
 var fs = require('fs');
 var dataDir = path.normalize(process.cwd() + '/data/');
 var fetch = require("fetch").fetchUrl;
-var _ = require("lodash");
 
 //Initialize base directory
 try{
@@ -11,10 +10,11 @@ fs.mkdirSync(dataDir + '/versions/');
 }catch(e){}
 
 //Fetch data
-console.log("Importing data from http://s3.amazonaws.com/Minecraft.Download/versions/versions.json");
-fetch("http://s3.amazonaws.com/Minecraft.Download/versions/versions.json", function(error, meta, body){
+console.log("Importing data from https://launchermeta.mojang.com/mc/game/version_manifest.json");
+fetch("https://launchermeta.mojang.com/mc/game/version_manifest.json", function(error, meta, body){
 	//Fail on fetch error
 	if(error){
+        console.error("Failed to fetch json");
 		console.error(error);
 		process.exit(1);
 	}
@@ -24,18 +24,17 @@ fetch("http://s3.amazonaws.com/Minecraft.Download/versions/versions.json", funct
 
 	//Make a magic future version as a basis for any snapshots.
 	versionData.versions.push({
-
       "id": semver.inc(versionData.latest.release,"minor"),
-      "time": (function(){now = new Date();now.setDate(now.getDate() + 365);return now.toUTCString()})(),
-      "releaseTime": (function(){now = new Date();now.setDate(now.getDate() + 365);return now.toUTCString()})(),
+      "time": (function(){var now = new Date();now.setDate(now.getDate() + 365);return now.toUTCString()})(),
+      "releaseTime": (function(){var now = new Date();now.setDate(now.getDate() + 365);return now.toUTCString()})(),
       "type": "release",
       "__future_version__": true //DO NOT REMOVE, USED TO HIDE RELEASE FROM BEING OUTPUT
     });
 
 	//Only find snapshots and releases
-	var filteredData = _(versionData.versions)
+	var filteredData = versionData.versions
 	.filter(function(e){ return ['release','snapshot'].indexOf(e.type) !== -1})
-	.each(function(e){
+	.map(function(e){
 		//Patch bad version numbers to semver major.minor.patch format
 		if(e.id.split(".").length == 2){
 			console.log("patching",e.id);
@@ -43,13 +42,13 @@ fetch("http://s3.amazonaws.com/Minecraft.Download/versions/versions.json", funct
 		}
 		//Convert release time for sorting.
 		e.time = new Date(e.releaseTime);
-	})
-	.value()
+        return e;
+	});
 
 	//Find the earliest version of each release (the 1.x.0's in most cases,)
 	//And store for use in snapshot semver fixing.
-	var releasesOnly = _.chain(filteredData)
-	.filter(function(e){ return e.type == 'release';})
+    console.log(filteredData);
+	var releasesOnly = filteredData.filter(function(e){ return e.type == 'release';})
 	.reduce(function(acc,e){
 		var id = e.id.split(".");
 		var minorVersion = id[0] + '.' + id[1];
@@ -63,13 +62,10 @@ fetch("http://s3.amazonaws.com/Minecraft.Download/versions/versions.json", funct
 			acc[minorVersion] = e;
 		}
 		return acc
-	},{})
-	.values()
-	.value();
+	},{});
 
 	//Dump out all versions to the filesystem
-	var versionEntries = _(filteredData)
-	.filter(function(e){ return !e.__future_version__;}) //EXCLUDE OUR VERSION WE MADE FOR SNAPSHOT
+	filteredData.filter(function(e){ return !e.__future_version__;}) //EXCLUDE OUR VERSION WE MADE FOR SNAPSHOT
 	.map(function(e){
 		//Fix snapshot versions to major.minor.patch-snapshot format.
 		var versionId = e.id;
@@ -86,14 +82,26 @@ fetch("http://s3.amazonaws.com/Minecraft.Download/versions/versions.json", funct
 			type: e.type,
 			released: e.time
 		}
-	}).sortBy("time").each(function(e,idx,col){ //Sort by time released.
+	}).reduce(function(out, i){
+        out.push(i);
+        out.sort(function(a,b){
+            if(a.time == b.time){
+                return 0;
+            }
+            if(a.time > b.time){
+                return 1;
+            }else{
+                return -1;
+            }
+        });
+        return out;
+    },[]).forEach(function(e,idx,col){ //Sort by time released.
 		if(idx > 0){
 			if(semver.lt(col[idx-1].id,e.id)){
 				//Check for release time weirdness
 				console.error("temporal oddity detected",col[idx-1].id,"released after",e.id);
 			}
 		}
-	}).each(function(e){
 		//If file doesn't exist, write to disk.
 		try{
 			fs.statSync(dataDir + '/versions/' + e.id + '.json');
@@ -101,6 +109,6 @@ fetch("http://s3.amazonaws.com/Minecraft.Download/versions/versions.json", funct
 			console.log("Storing new version", e.id);
 			fs.writeFileSync(dataDir + '/versions/' + e.id + '.json',JSON.stringify(e,null,4));
 		}
-	}).value();
+	})
 	
 });
