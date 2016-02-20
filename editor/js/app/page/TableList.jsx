95df44e76bbb192695481c70aaae67a0a6bb933b
James <beardyjames@live.co.uk>
2016-02-20 21:09:58 +0000

    Work on making the table sortable. Improve filtering.

--- editor/js/app/page/TableList.jsx
+++ editor/js/app/page/TableList.jsx
@@ -1,46 +1,79 @@
 "use strict";
-
+import update from 'react-addons-update';
 import React from 'react';
+import {compare} from 'semver'; 
 
 export default class TableList extends React.Component {
     constructor()
     {
         super();
-        this.state = {filter:null};
+        this.state = {
+            filter: new RegExp(""),
+            sortFn: ()=>0
+        };
+        this.fltrRel = (a,b)=>compare(b.introduced_at,a.introduced_at);
+        this.fltrChanged = (a,b)=>compare(b.changed_at,a.changed_at);
+        this.fltrId = (a,b)=>{
+            if(a.id > b.id){
+                return 1
+            }else if(a.id < b.id){
+                return -1;
+            }
+            return 0;
+        }
     }
     render(){
         return <div>
         <input type="text" id="filter" onKeyUp={(ev)=>{
             console.log(ev.target.value);
-        this.setState({
-            filter: ev.target.value
-        });
+        this.setState(
+            update(
+                this.state,
+                {filter: {$set: new RegExp(ev.target.value)} }
+            )
+        );
         }}/>
     <table className="table">
     <thead>
     <tr>
     <th>Mod</th>
-    <th>Id</th>
-    <th>Released</th>
-    <th>Updated</th>
+    <th onClick={this.filterHandler(this.fltrId)}>Id</th>
+    <th onClick={this.filterHandler(this.fltrRel)}>Released</th>
+    <th onClick={this.filterHandler(this.fltrChanged)}>Updated</th>
     <th>Edit</th>
     </tr>
     </thead>
     <tbody id="entries">
-    {this.props.entries.filter((e)=>( !this.state.filter || e.mod.indexOf(this.state.filter) !== -1 || e.id.indexOf(this.state.filter) !== -1)).map((e)=><tr key={`${this.props.tableName}-${e.mod}-${e.id}`}>
-      <td>{e.mod}</td>
-      <td>{e.id}</td>
-      <td>{e.introduced_at}</td>
-      <td>{e.changed_at}</td>
-      <td><a href={"#/" + this.props.tableName + "/" + e.mod + "/" + e.id} className="btn btn-primary edit">Edit</a></td>
-    </tr>)}
+    {this.props.entries
+        .filter((e)=>this.state.filter.test(e.mod) || this.state.filter.test(e.id))
+        .concat()
+        .sort(this.state.sortFn)
+        .map((e)=>
+            <tr key={`${this.props.tableName}-${e.mod}-${e.id}`}>
+                <td>{e.mod}</td>
+                <td>{e.id}</td>
+                <td>{e.introduced_at}</td>
+                <td>{e.changed_at}</td>
+                <td><a href={"#/" + this.props.tableName + "/" + e.mod + "/" + e.id} className="btn btn-primary edit">Edit</a></td>
+            </tr>)
+    }
     </tbody>
     </table>
         </div> 
     }
     
-    onFilter(ev){
-        
+    filterHandler(hnd){
+        return ()=>{
+            let  flip = this.state.sortFn === hnd;
+            console.log("CLUNK", flip ? "NORMAL" : "INVERT")
+            console.log("CURRENT", this.state.sortFn);
+            console.log("replacement",hnd);
+            this.setState(
+                update(this.state,
+                { sortFn: {$set: ( flip ? function(a,b){return 0-hnd(a,b);} : hnd ) } }
+                )
+            )
+        }
     }
         
 }
