150df263b8bd4d29d719be01232ae7e07af4bb19
James <beardyjames@live.co.uk>
2016-03-20 18:02:30 +0000

    work on validate system.

--- validate-database.js
+++ validate-database.js
@@ -4,11 +4,27 @@ var Validator = require('jsonschema').Validator;
 
 
 function loadJSON(filename, cb) {
-    fs.readFile(path.join('./schemas', filename), 'ascii', function(err, body) {
+    if (!cb) { return loadJSON.bind(this, filename); }
+    fs.readFile(filename, 'ascii', function(err, body) {
         if (err) {
             return cb(err);
         }
-        cb(null, JSON.parse(body));
+        try{
+            cb(null, JSON.parse(body));
+        }catch(e){
+            console.error("ERROR PARSING FILE", filename);
+            console.error(e);
+        }
+    });
+}
+
+function saveJSON(filename,data, cb) {
+    if (!cb) { return saveJSON.bind(this, filename, data); }
+    fs.writeFile(filename, JSON.stringify(data, null, 2), function(err, body) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null);
     });
 }
 
@@ -17,16 +33,16 @@ function getValidator(initialSchema, cb) {
     var Validator = require('jsonschema').Validator;
     var v = new Validator();
 
-    loadJSON(initialSchema, function(err, schema) {
+    loadJSON(path.join('./schemas', initialSchema), function(err, schema) {
         if (err) {
             return cb(err);
         }
-        v.addSchema(schema,initialSchema);
+        v.addSchema(schema, initialSchema);
         function importNextSchema() {
             var nextSchema = v.unresolvedRefs.shift();
-            if (!nextSchema) { cb(null,initialSchema, v); return; }
+            if (!nextSchema) { cb(null, initialSchema, v); return; }
             console.log("RESOLVING", nextSchema);
-            loadJSON(nextSchema, function(err, schema) {
+            loadJSON(path.join('./schemas', nextSchema), function(err, schema) {
                 if (err) {
                     return cb(err);
                 }
@@ -37,16 +53,40 @@ function getValidator(initialSchema, cb) {
         }
         importNextSchema();
     })
-
-
-
-
-
 }
-var data = JSON.parse(fs.readFileSync('data/blocks/minecraft/wool.json','ascii'))
-getValidator('/block.json', function(err,schema,v){
+var run = require('gen-run');
+
+getValidator('/block.json', function(err, schema, v) {
     console.log(err);
-    console.log(
-        v.validate(data,'/block.json')
-    );
+    run(function* () {
+        var valid = 0, invalid = 0;
+        var files = yield fs.readdir.bind(fs, 'data/blocks/minecraft/');
+        files = files.map((e) => `./data/blocks/minecraft/${e}`);
+        for (x in files) {
+            json = yield loadJSON(files[x]);
+            res = v.validate(json, '/block.json');
+            if (!res.valid) {
+                invalid++;
+                console.log(files[x]);
+                res.errors.forEach((err) => {
+                    console.log(`${err.property} ${err.message}`);
+                });
+                
+                json.meta = Object.keys(json.meta).map((key)=>{
+                    return {
+                        key: key,
+                        values: Object.keys(json.meta[key]).map((value)=>{ return {value:value, mask: json.meta[key][value]}; })
+                    };
+                })
+                yield saveJSON(files[x], json);
+                
+                console.log();
+            } else {
+                valid++;
+                //console.log(files[x],"VALID");
+            }
+        }
+        console.error("Valid", valid, valid + invalid);
+        console.error("Invalid", invalid, valid + invalid);
+    })
 })
