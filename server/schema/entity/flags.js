ee8c4999d2fec99e194fba89ac9242fcc43878b4
James <beardyjames@live.co.uk>
2016-09-23 22:31:00 +0100

    Implement entity support

---
+++ server/schema/entity/flags.js
@@ -0,0 +1,23 @@
+var graphql = require('graphql');
+
+module.exports = new graphql.GraphQLObjectType({
+    name: "EntityFlags",
+    fields: {
+        durability: {
+            description: "Amount of damage",
+            type: graphql.GraphQLInt
+        },
+        dyeable: {
+            description: "Item can be coloured",
+            type: graphql.GraphQLBoolean
+        },
+        repairable: {
+            description: "Can be smashed in an anvil to fix it.",
+            type: graphql.GraphQLBoolean
+        },
+        enchantments: {
+            description: "valid enchantments for this item", 
+            type: new graphql.GraphQLList( require("../Enchantment")) 
+        }
+    }
+});
--- server/schema/index.js
+++ server/schema/index.js
@@ -6,10 +6,12 @@ var path = require('path');
 
 let itemDB = new Database(path.resolve("./data/items"));
 let blockDB = new Database(path.resolve("./data/blocks"));
+let entityDB = new Database(path.resolve("./data/entities"));
 let enchantmentDB = new Database(path.resolve("./data/enchantments"));
 
 let Block = require("./block");
 let Item = require("./item");
+let Entity = require("./entity");
 let Enchantment = require("./Enchantment");
 
 module.exports = new graphql.GraphQLSchema({
@@ -18,6 +20,7 @@ module.exports = new graphql.GraphQLSchema({
         fields: {
             items: { args:{id: {type:graphql.GraphQLString}}, type: new graphql.GraphQLList(Item), resolve: (_,{id}) => itemDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id)) },
             blocks: {  args:{id: {type:graphql.GraphQLString}}, type: new graphql.GraphQLList(Block),  resolve: (_,{id}) => blockDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id))},
+            entities: {  args:{id: {type:graphql.GraphQLString}}, type: new graphql.GraphQLList(Entity),  resolve: (_,{id}) => entityDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id))},
             enchantments: {  args:{id: {type:graphql.GraphQLString}}, type: new graphql.GraphQLList(Enchantment),  resolve: (_,{id}) => enchantmentDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id))} 
         }
     })
