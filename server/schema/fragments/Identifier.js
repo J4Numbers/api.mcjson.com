66e020d3680983b0827de7643cdb80f2e19762dd
James <beardyjames@live.co.uk>
2016-09-23 22:32:29 +0100

    Add flag to output id in mod:id format

--- server/schema/fragments/Identifier.js
+++ server/schema/fragments/Identifier.js
@@ -1,7 +1,14 @@
 var graphql = require('graphql');
 
-module.exports = {fields:{
-      mod: {type: graphql.GraphQLString, description: "Mod this object comes from"} ,
-      id: {type: graphql.GraphQLString, description: "Id of this object, combined with mod in format mod:id"} ,
-      name: {type: graphql.GraphQLString, description: "English name of object"}
-  }}
+module.exports = {
+  fields: {
+    mod: { type: graphql.GraphQLString, description: "Mod this object comes from" },
+    id: {
+      args: { prefixMod: { type: graphql.GraphQLBoolean, defaultValue: false } },
+      type: graphql.GraphQLString,
+      resolve: (_,{prefixMod}) => ( (prefixMod ? `${_.mod}:`:"") +  _.id),
+      description: "Id of this object, combined with mod in format mod:id"
+    },
+    name: { type: graphql.GraphQLString, description: "English name of object" }
+  }
+}
