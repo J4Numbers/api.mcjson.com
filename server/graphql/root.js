4b5dbc04194be777e05a245fd4c8261216f750d2
James <beardyjames@live.co.uk>
2016-10-01 22:14:34 +0100

    more poke stuff

--- server/graphql/root.js
+++ server/graphql/root.js
@@ -5,26 +5,32 @@ let blockDB = databases.blockDB;
 let entityDB = databases.entitiesDB;
 let enchantmentDB = databases.enchantmentsDB;
 let versionsDB = databases.versionsDB;
-let dm = (...mixins) => (obj) => Object.assign({},obj,mixins.reduce(deepmerge,{})); 
+let dm = (...mixins) => (obj) => { 
+    var o = Object.assign({},mixins.reduce(deepmerge,{}));
+    o._ = obj;
+    return o;
+}; 
+
+var ItemModel = dm({
+                id({prefixMod}){
+                    return (prefixMod ? this._.mod + ":":"") + this._.id;
+                }
+            });
 
 module.exports = Object.assign({
     items({id}) {
         return itemDB.entries.then(
-            items => items.map(e => e.data()).filter(e => id == null || e.id == id).map( dm({
-                id({prefixMod}){
-                    return this.prototype.id
-                }
-            }) )
+            items => items.map(e => e.data()).filter(e => id == null || e.id == id).map( ItemModel )
         );
     },
     blocks({id}) {
-        return blockDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id));
+        return blockDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id).map( ItemModel ));
     },
     entities({id}) {
-        return entityDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id));
+        return entityDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id).map( ItemModel ));
     },
     enchantments({id}) {
-        return enchantmentDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id));
+        return enchantmentDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id).map( ItemModel ));
     },
     versions({id, type}) {
         return versionsDB.entries.then(items => items.map(e => e.data()).filter(e => id == null || e.id == id).filter(e => type == null || e.type == type))
