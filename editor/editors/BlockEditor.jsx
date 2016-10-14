adaee2e113c44ef10904831940d4c418f3cf221c
James <beardyjames@live.co.uk>
2016-10-14 23:35:18 +0100

    Work on rename support

--- editor/editors/BlockEditor.jsx
+++ editor/editors/BlockEditor.jsx
@@ -24,7 +24,13 @@ export default class BlockEditor extends React.Component {
         });
     }
     onSave() {
-        setBlock({oldId:{mod: this.props.params.mod, id: this.props.params.id}, newData: this.state.data}).then(()=>this.setState({isDirty:false}));
+        setBlock({oldId:{mod: this.props.params.mod, id: this.props.params.id}, newData: this.state.data}).then((data)=>{
+            this.setState({isDirty:false});
+            if(location.hash!= `/blocks/${data.updateBlock.mod}/${data.updateBlock.id}`){ 
+                location.hash = `/blocks/${data.updateBlock.mod}/${data.updateBlock.id}`;
+            }
+
+        });
     }
     render() {
         return <div>
