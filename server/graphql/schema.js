89e040d861ed147f459a7c19d622b421c3e57945
James <beardyjames@live.co.uk>
2016-10-15 22:40:39 +0100

    Refactor GQL in server portion so it can be split up for sanity.
    Add a NBTEditors enum to the meta for Block and Item, this will be used to declare which additional complex NBT structures an editor should consider.

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -2,11 +2,17 @@ var gql = require("graphql");
 var fs = require("fs");
 var path = require("path");
 
-module.exports = gql.buildSchema([
+function collateSchemas(dir){
+  return fs.readdirSync(dir).map( f => fs.readFileSync(path.resolve(dir,f),"ascii") ).join("\n");
+}
+
+module.exports = gql.buildSchema(  
+  [
+    collateSchemas(path.resolve(__dirname,'./schema')),
 `schema {
   query: query
   ${process.env.NODE_ENV != 'production' ? 'mutation: mutation' : ''}
-}`,
-fs.readFileSync(path.resolve(__dirname,"./schema.gql"),"ascii"),
-process.env.NODE_ENV != 'production' ? fs.readFileSync(path.resolve(__dirname,"./editor.gql"),"ascii") : "",
+}`
+,
+process.env.NODE_ENV != 'production' ? fs.readFileSync(path.resolve(__dirname,"./schema.editor/editor.gql"),"ascii") : "",
 ].join("\n"));
