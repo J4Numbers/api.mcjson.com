c126c21acb695a95111baea0961c110e8b7eeb63
james <james@ballyhoo.co.uk>
2016-11-24 16:23:27 +0000

    doc and make runtime pluggable.

--- editor/web_loaders/graphql.js
+++ editor/web_loaders/graphql.js
@@ -1,12 +1,17 @@
+/**
+ * Transform GQL query files into callable functions
+ */
 var loaderUtils = require('loader-utils');
 module.exports = function (source) {
     this.cacheable && this.cacheable();
+    var qry = loaderUtils.parseQuery(this.query);
     var options = Object.assign({
         url: "/graphql",
         headers:{}
-    },loaderUtils.parseQuery(this.query));
-
-    var req = loaderUtils.stringifyRequest(this, "!" + require.resolve("./graphql-runtime"));
+    },qry.options);
+    var runPath = require.resolve( qry.runtime || "./graphql-runtime");
+    this.addDependency(runPath)
+    var req = loaderUtils.stringifyRequest(this, "!" + runPath);
     var src = `(function(){
         var runtime = require(${req});
         return runtime(\`${source}\`,${JSON.stringify(options)})
