db79e4b72f19f6cf2fe0a6b54b8de3f2c6e7646c
James <beardyjames@live.co.uk>
2016-12-20 22:44:55 +0000

    both sides need to match to work.

--- server/graphql/models/index.js
+++ server/graphql/models/index.js
@@ -1,9 +1,14 @@
-module.exports = {
+let root = {
     Query: require("./Query.js"),
     Item: require("./idPrefixMod.js"),
     Block: require("./idPrefixMod.js"),
     Entity: require("./idPrefixMod.js"),
     Enchantment: require("./idPrefixMod.js"),
     StatusEffect: require("./idPrefixMod.js"),
-    Mutation: require("./Mutation.js")
 }
+
+if(process.env.NODE_ENV != 'production'){
+    root.Mutation =  require("./Mutation.js");
+}
+
+module.exports = root;
