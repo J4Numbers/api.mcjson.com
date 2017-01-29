d92f9b517a90c6c705176443982668355f964131
James <beardyjames@live.co.uk>
2017-01-29 11:20:59 +0000

    fix test for .js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.js?$/,
+                test: /\.js$/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2015']
