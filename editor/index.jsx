8c3ceefe9a77b54e783a5f007ac009c78d1c756f
James <beardyjames@live.co.uk>
2017-01-30 23:00:28 +0000

    clean up editor to build.

--- editor/index.jsx
+++ editor/index.jsx
@@ -5,7 +5,6 @@ import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-ro
 import TableList from './editors/TableList.jsx';
 // import VersionEditor from './views/VersionEditor.jsx';
  import EnchantmentEditorPage from './pages/EnchantmentEditorPage.jsx';
-import BlockEditorPage from './pages/BlockEditorPage.jsx';
 import ItemEditorPage from './pages/ItemEditorPage.jsx';
 import EntityEditorPage from './pages/EntityEditorPage.jsx';
 import StatusEffectEditorPage from './pages/StatusEffectEditorPage.jsx';
@@ -18,8 +17,6 @@ loadVersionData().then(() => {
         <Route path="/" component={App} >
             {/*<Route path="/versions/:version" component={VersionEditor} />*/}
             <Route path="/enchantments/:mod/:id" component={EnchantmentEditorPage} />
-            <Route path="/blocks/:mod/:id" component={BlockEditorPage} />
-            <Route path="/blocks/_new" component={BlockEditorPage} />
             <Route path="/items/:mod/:id" component={ItemEditorPage} />
             <Route path="/items/_new" isNew={true} component={ItemEditorPage} />
             <Route path="/entities/:mod/:id" component={EntityEditorPage} />
