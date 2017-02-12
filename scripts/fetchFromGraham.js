49ad092fdb2b825e2c8ff9efb020fe6ed027db7a
James <beardyjames@live.co.uk>
2017-02-12 16:18:15 +0000

    Fold block and items in graham item import into one to match data structure.

--- scripts/fetchFromGraham.js
+++ scripts/fetchFromGraham.js
@@ -7,56 +7,25 @@ console.log("Importing data from http://minecraft-ids.grahamedgecombe.com/");
 
 fetch("http://minecraft-ids.grahamedgecombe.com/items.json").then(res => res.json()).then(itemData => {
     run(function* () {
-        var files = yield fs.readdir.bind(fs, './data/blocks/minecraft');
-        files = files.map((file) => file.split(".")[0])
-        var newMappings = itemData
-            .filter((item) => files.indexOf(item.text_type) === -1) //Filter existing
-            .filter((item) => item.type < 256) //Filter to blocks
-            .reduce((newBlocks, entry) => {
-                if (!newBlocks[entry.text_type]) {
-                    newBlocks[entry.text_type] = {
-                        mod: "minecraft",
-                        id: entry.text_type,
-                        name: entry.name,
-                        "technical": false,
-                        "introduced_at": "1.11.0",
-                        "changed_at": "1.11.0",
-                        "meta": [{
-                            key: "meta",
-                            values: []
-                        }]
-                    }
-                }
-                newBlocks[entry.text_type].meta[0].values.push({ value: entry.name, mask: entry.meta });
-                return newBlocks;
-            }, {});
-        for (fName in newMappings) {
-            if (newMappings[fName].meta[0].values.length == 1) {
-                delete newMappings[fName].meta;
-            }
-            console.log("Writing out new block", fName);
-            yield fs.writeFile.bind(fs, path.resolve('./data/blocks/minecraft', fName + '.json'), JSON.stringify(newMappings[fName], null, 2));
-        }
-    });
-
-    run(function* () {
         var files = yield fs.readdir.bind(fs, './data/items/minecraft');
         files = files.map((file) => file.split(".")[0])
         var newMappings = itemData
             .filter((item) => files.indexOf(item.text_type) === -1) //Filter existing
-            .filter((item) => item.type >= 256) //Filter to blocks
             .reduce((newBlocks, entry) => {
                 if (!newBlocks[entry.text_type]) {
                     newBlocks[entry.text_type] = {
                         mod: "minecraft",
                         id: entry.text_type,
                         name: entry.name,
-                        "introduced_at": "1.9.0",
-                        "changed_at": "1.9.0",
+                        "introduced_at": "1.12.0",
+                        "changed_at": "1.12.0",
                         "meta": [{
                             key: "meta",
                             values: []
-                        }]
+                        }],
+                        flags:{
+                            isBlock: item.type < 256
+                        }
                     }
                 }
                 newBlocks[entry.text_type].meta[0].values.push({ value: entry.name, mask: entry.meta });
