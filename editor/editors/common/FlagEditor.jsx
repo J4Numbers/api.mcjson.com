1b5994744883e1a5684f080ea54a356eea4fa560
James <beardyjames@live.co.uk>
2017-02-25 17:38:39 +0000

    Fix up inventory editor.

--- editor/editors/common/FlagEditor.jsx
+++ editor/editors/common/FlagEditor.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import update from 'immutability-helper';
 /**
 # Is this item a block
   isBlock: Boolean!
@@ -47,7 +48,7 @@ type BlockFlagsLight {
 
 export function FlagCheckbox(props) {
     return <div className="form-group"><div className="checkbox">
-        <label><input type="checkbox" checked={props.value} onChange={(ev) => { props.onChange(ev.target.checked ? true : undefined) } } /> {props.label}</label>
+        <label><input type="checkbox" checked={props.value} onChange={(ev) => { props.onChange(ev.target.checked ? true : undefined) }} /> {props.label}</label>
     </div></div>
 }
 
@@ -57,57 +58,89 @@ export function FlagInt(props) {
         <div className="input-group">
             <span className="input-group-addon">
                 <input type="checkbox" onChange={(ev) => {
-                    props.onChange(ev.target.checked ? 0 : undefined );
-                } } checked={!(props.value == undefined)} />
+                    props.onChange(ev.target.checked ? 0 : undefined);
+                }} checked={!(props.value == undefined)} />
             </span>
             <input className="form-control" readOnly={(props.value == undefined)} type="number" min="0" step="1" value={props.value || 0} onChange={(ev) => props.onChange(parseInt(ev.target.value))}
-                />
+            />
         </div>
     </div>
 }
 
-function toggleArray(arr, val){
-    if(arr.indexOf(val) !== -1){
+function toggleArray(arr, val) {
+    if (arr.indexOf(val) !== -1) {
         let a = arr.slice();
-        a.splice(arr.indexOf(val),1);
+        a.splice(arr.indexOf(val), 1);
         return a;
-    }else{
+    } else {
         return arr.concat([val]);
     }
 }
 
 export function BlockFlagEditor(props) {
-    const update = (data)=>{
-        props.onChange(Object.assign({}, props.data, data));
+    const upd = (data) => {
+        props.onChange(update(props.data, data));
     }
     return <div className="block-flag">
-        <FlagCheckbox label="Physics" value={props.data ? props.data.physics : false} onChange={ c => update({ physics: c }) } />
-        <FlagInt label="Inventory slots" value={props.data ? props.data.inventory_slots : 0} onChange={ slots => update({ inventory_slots: slots }) } />
+        <FlagCheckbox label="Physics" value={props.data ? props.data.physics : false} onChange={c => upd({ physics: {$set: c } })} />
         <fieldset>
-        <legend>NBT data editors</legend>
-        <FlagCheckbox label="Shulker Colour picker" value={props.data && props.data.editors && props.data.editors.indexOf("SHULKER_BOX")!==-1} onChange={(v) => update( { editors: toggleArray(props.data.editors||[],"SHULKER_BOX") } ) } />
-        
+            <legend>
+                <input
+                    type="checkbox"
+                    checked={props.data.inventory != undefined}
+                    onChange={(ev) => {
+                        upd({ inventory: {$set: ev.target.checked ? { slots: 0, type: "CHEST" } : undefined } })
+                    }}
+                />
+                inventory
+            </legend>
+            <div className="form-group">
+                <label>Slots: </label>
+                <input
+                className="form-control"
+                readOnly={(props.data.inventory == undefined)}
+                type="number"
+                min="0"
+                step="1"
+                value={props.data.inventory ? props.data.inventory.slots : 0}
+                onChange={(ev) => upd({inventory: {slots: {$set: parseInt(ev.target.value) } } })}
+                    />
+            </div>
+            <div className="form-group">
+                <label>Type: </label>
+                <select
+                className="form-control"
+                readOnly={(props.data.inventory == undefined)}
+                value={props.data.inventory ? props.data.inventory.type : ""}
+                onChange={(ev) => upd({inventory: {type: {$set: ev.target.value } } })}
+                >
+                <option value="CHEST">Chest</option>
+                <option value="DISPENSER">Dispenser</option>
+                </select>
+            </div>
+
+
         </fieldset>
 
     </div>
 }
 
 export function ItemFlagEditor(props) {
-    const update = (data)=> props.onChange(Object.assign({}, props.data, data));
+    const upd = (data) => props.onChange(Object.assign({}, props.data, data));
     return <div className="block-flag">
-        <FlagInt label="Durability" value={props.data ? props.data.durability : 0} onChange={ slots => update({ durability: slots }) } />
-        <ItemNBT value={ (props.data && props.data.editors !== undefined ? props.data.editors: [])} onChange={ editors => update({ editors}) }  />
+        <FlagInt label="Durability" value={props.data ? props.data.durability : 0} onChange={slots => upd({ durability: {$set: slots} })} />
+        <ItemNBT value={(props.data && props.data.editors !== undefined ? props.data.editors : [])} onChange={editors => upd({ editors: {$set: editors } })} />
     </div>
 }
 
-function ItemNBT(props){
-    let bind = entry => ({ value: (props.value || []).indexOf(entry)!== -1, onChange: (v)=> props.onChange(toggleArray(props.value||[],entry))})
+function ItemNBT(props) {
+    let bind = entry => ({ value: (props.value || []).indexOf(entry) !== -1, onChange: (v) => props.onChange(toggleArray(props.value || [], entry)) })
     return <fieldset>
         <legend>NBT data editors</legend>
-        <FlagCheckbox label="Potion Effects" {...bind("POTION")} />
-        <FlagCheckbox label="Book Contents" {...bind("BOOK")} />
-        <FlagCheckbox label="Book Author" {...bind("AUTHOR")} />
-        <FlagCheckbox label="Banner" {...bind("BANNER")} />
-        <FlagCheckbox label="Firework" {...bind("FIREWORK")} />
-        </fieldset>
+        <FlagCheckbox label="Potion Effects" {...bind("POTION") } />
+        <FlagCheckbox label="Book Contents" {...bind("BOOK") } />
+        <FlagCheckbox label="Book Author" {...bind("AUTHOR") } />
+        <FlagCheckbox label="Banner" {...bind("BANNER") } />
+        <FlagCheckbox label="Firework" {...bind("FIREWORK") } />
+    </fieldset>
 }
