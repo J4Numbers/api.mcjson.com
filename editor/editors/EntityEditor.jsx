e8d5989e2bc9eb31ffc938241690b6de3de0eed9
James <beardyjames@live.co.uk>
2017-03-11 20:17:47 +0000

    Remove GUI editor from this package, will make it's own thing.

--- editor/editors/EntityEditor.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {BaseEditor} from './common/BaseEditor.jsx';
-
-export default function EntityEditor(props) {
-    return <div>
-            <BaseEditor value={props.value} onChange={props.onChange}/>
-            <pre>{JSON.stringify(props.value, null,2)}</pre>
-        </div>
-}
--- server/index.js
+++ server/index.js
@@ -27,24 +27,6 @@ if (process.env.NODE_ENV != 'production') {
     app.use(morgan('common'));
 }
 
-if (process.env.NODE_ENV != 'production') {
-    var webpack = require("webpack");
-    var webpackDevMiddleware = require('webpack-dev-middleware');
-    var webpackHotMiddleware = require('webpack-hot-middleware');
-    var config = require('../webpack.config.js');
-    var compiler =  webpack(config);
-    app.use(webpackDevMiddleware(
-        compiler,
-        {
-            publicPath: config.output.publicPath,
-            stats:{colors:true}
-        }
-    ));
-    app.use(webpackHotMiddleware(compiler, {
-        log: console.log
-    }))
-}
-
 app.use ('/', require("./graphql/http.js"));
 
 // app.use((req,res)=>{
